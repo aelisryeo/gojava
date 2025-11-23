894f7918a766f57e48a40b7f381ec6dbae90aa09
khrr042 <khrr0420@gmail.com>
2025-11-24 02:37:23 +0900

    설명창수정(그냥내용만...)

--- untitled/src/RulesPanel.java
+++ untitled/src/RulesPanel.java
@@ -20,22 +20,22 @@ public class RulesPanel extends JPanel implements GameConstants {
         add(titleLabel, BorderLayout.NORTH);
 
         String rulesText =
-                "## 📌 조작 방법\n\n" +
+                "##  조작 방법\n" +
                         " - 방향 전환: 화면에 표시된 방향키를 눌러 진행 방향을 바꿉니다.\n" +
-                        " - 전진: 스페이스바를 누르거나, 방향키를 누를 때마다 캐릭터가 전진합니다.\n" +
-                        " - 점프: 스페이스바를 길게 눌러 캐릭터 위의 게이지가 전부 차면 점프를 합니다.\n\n" +
-                        "## ⚠️ 게임 오버의 경우\n\n" +
+                        " - 전진: 스페이스바를 누르거나, 방향키를 누르면 캐릭터가 전진합니다.\n" +
+                        " - 점프: 스페이스바를 길게 누르면 게이지가 채워지고, 손을 떼면 점프합니다.\n\n" +
+                        "## 게임 오버의 경우\n" +
                         " 1. 잘못된 방향으로 움직여 계단 경로를 이탈했을 때\n" +
                         " 2. 주어진 제한 시간 내에 계단을 밟지 못했을 때\n" +
                         " 3. 생명이 0이 되었을 때\n\n" +
-                        "## 아이템\n\n" +
+                        "## 아이템\n" +
                         " - 시계 획득: 시간 제한이 잠시 느려집니다.\n" +
                         " - 시험지 획득: 잠시 동안 점수가 두 배가 됩니다.\n" +
                         " - 하트 획득: 생명이 증가합니다.\n\n"+
-                        "## 장애물\n\n" +
-                        " - 교수님: 점수가 깎입니다. 점프로 피하십시오.\n" +
-                        " - 학생: 화면에 표시되는 문자열을 입력하시오.\n" +
-                        " - 버섯: 화면에 표시되는 키를 연타하시오.";;
+                        "## 장애물\n" +
+                        " - 교수님: 점수가 깎입니다. 점프로 피하세요.\n" +
+                        " - 학생: 화면에 표시되는 문자열을 입력하세요.\n" +
+                        " - 버섯: 화면에 표시되는 키를 연타하세요.";;
 
         JTextArea rulesArea = new JTextArea(rulesText);
         rulesArea.setFont(GameFont.getFont( Font.PLAIN, 18));
@@ -55,7 +55,7 @@ public class RulesPanel extends JPanel implements GameConstants {
 
         JButton backButton = createStyledButton("돌아가기", 20, new Color(200, 149, 237));
         backButton.setFont(GameFont.getFont( Font.PLAIN, 20));
-        backButton.setForeground(Color.BLACK);
+        backButton.setForeground(Color.WHITE);
         backButton.setBackground(new Color(237, 237, 237));
         backButton.setFocusPainted(false);
         backButton.setActionCommand("BACK_TO_START");
