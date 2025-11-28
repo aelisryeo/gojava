e6f0c798001ac4f8dd6d3dac4d7c9324c0633db8
khrr042 <khrr0420@gmail.com>
2025-11-28 18:01:29 +0900

    코드정리

--- untitled/src/GameOverPanel.java
+++ untitled/src/GameOverPanel.java
@@ -2,8 +2,6 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 
 public class GameOverPanel extends JPanel implements ActionListener, GameConstants {
